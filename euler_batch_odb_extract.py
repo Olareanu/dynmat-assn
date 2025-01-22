a54b7fc84e2770f8acf1a319b11dd5b26f577ab8
Olareanu Alexandru <47012278+Olareanu@users.noreply.github.com>
2025-01-22 16:40:29 +0100

    Better default job name list in euler_batch_odb_extract.py

--- euler_batch_odb_extract.py
+++ euler_batch_odb_extract.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
 
+
 def submit_jobs():
     # Define an internal list of job names. If the string is contained in the name, it will get its own sbatch
-    job_names = ["SV-1-", "SV-23-"]  # Replace these with your actual job names
+    job_names = ["BAV-15-BRV-5", "BAV-15-BRV-8", "BAV-15-BRV-10", "BAV-30-BRV-5", "BAV-30-BRV-8", "BAV-30-BRV-10",
+                 "BAV-45-BRV-5", "BAV-45-BRV-8", "BAV-45-BRV-10"]  # Replace these with your actual job names
 
     # Parameters for the sbatch script creation
     num_cpus = 2
@@ -25,10 +27,12 @@ def submit_jobs():
         # Execute the sbatch command
         try:
             # Use subprocess to run the sbatch command
-            result = subprocess.run(sbatch_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            result = subprocess.run(sbatch_command, shell=True, check=True, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
             print(f"Submitted job '{job_name}', SLURM response: {result.stdout.decode().strip()}")
         except subprocess.CalledProcessError as e:
             print(f"Failed to submit job '{job_name}'. Error: {e.stderr.decode().strip()}")
+
 
 if __name__ == "__main__":
     submit_jobs()
